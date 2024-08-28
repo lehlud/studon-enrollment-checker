276023c47c936f96c88754ed25f42b37f1cfb8a3
Ludwig Lehnert <github@ludwig-lehnert.de>
2024-08-28 18:37:52 +0200

    using requests instead of selenium now

--- lib.py
+++ lib.py
@@ -1,72 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
-from dotenv import load_dotenv
-import smtplib
 import ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import os
 import json
 import time
 import requests
+from pyquery import PyQuery
+from dotenv import load_dotenv
+
+import smtplib
+from email.mime.text import MIMEText
 
 load_dotenv()
 
 
-def open_authed_studon_driver() -> WebDriver:
-    driver = open_driver()
-    driver.get('https://www.studon.fau.de/studon/saml.php')
-
-    while not driver.current_url.startswith('https://www.sso.uni-erlangen.de'):
-        driver.implicitly_wait(0.5)
-
-    if os.getenv('IDM_USER') and os.getenv('IDM_PASS'):
-        driver.find_element(By.ID, 'username')
-        driver.find_element(By.ID, 'username').send_keys(os.getenv('IDM_USER'))
-        driver.find_element(By.ID, 'password').send_keys(os.getenv('IDM_PASS'))
-        driver.find_element(By.ID, 'submit_button').click()
-
-    while not driver.current_url.startswith('https://www.studon.fau.de'):
-        driver.implicitly_wait(0.5)
-
-    return driver
-
-
-def open_driver() -> WebDriver:
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-
-    return webdriver.Firefox(options=options)
-
-
-def get_course_info_using_driver(driver: WebDriver, course_id) -> tuple[str, str]:
-    url = f'https://www.studon.fau.de/studon/goto.php?target=crs_{course_id}'
-
-    driver.get(url)
-
-    name = driver.find_element(
-        By.CLASS_NAME, 'media-heading'
-    ).text.strip()
-
-    status = driver.find_element(
-        By.XPATH, "//div[*[text()=\'Aufnahmeverfahren\']]/*[2]"
-    ).text.strip()
-
-    access = driver.find_element(
-        By.XPATH, "//div[*[text()=\'Zugriff\']]/*[2]"
-    ).text.strip()
-
-    return name, status, access
-
-
-def get_course_info(course_id):
+def get_course_info(course_id) -> tuple[str, str, str] | None:
     url = f'https://www.studon.fau.de/studon/goto.php?target=crs_{course_id}'
     response = requests.get(url)
 
-    html = response.text
+    if response.status_code != 200:
+        return None
 
-    # TODO
+    pq = PyQuery(response.text)
+
+    name = pq('.media-heading').children()[0].text.strip()
+    status = pq(':contains(Aufnahmeverfahren)')[-1].getnext().text.strip()
+    access = pq(':contains(Zugriff)')[-1].getnext().text.strip()
+
+    return name, status, access
 
 
 def try_notify_course_update(course_id, name, status, access):
